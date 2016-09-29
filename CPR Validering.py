c659c606bd7eaba4e895434a74622513def5163c
Bruger <claus.huynh@yahoo.dk>
2016-09-29 17:13:29 +0200

    changed

--- CPR Validering.py
+++ CPR Validering.py
@@ -3,7 +3,6 @@ cpr = input('Indsæt dit cpr dd/mm/åå/xxxx f.eks. 1234567890: ')
 checkDiget = (4, 3, 2, 7, 6, 5, 4, 3, 2, 1)
 moduloCheck = False
 dateCheck = False
-
 def validate(cpr):
     int(cpr)
     if len(cpr) != 10:
@@ -64,7 +63,7 @@ def validate(cpr):
 
     if moduloCheck == True:
         if dateCheck == True:
-        #gender
+        #køn
             if int(cpr[-1]) % 2 == 0:
                 print('Køn: Kvinde')
             else:
